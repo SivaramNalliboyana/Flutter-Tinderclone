751e177bb0485bfdc55317a629c40cef1eb4bb15
GrandmasterSivaram <nsivaramdav@gmail.com>
2021-02-25 16:32:39 +0100

    Added like functions

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:http/http.dart';
 import 'package:tinderclone/utils/fonts.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,6 +15,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   SwiperController swipercontroller = SwiperController();
   bool autoplay = false;
+  String username;
+  String bio;
+  String profilepic;
+  GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
+
+  initState() {
+    super.initState();
+    getcurrentuserdata();
+  }
 
   getusers() async {
     print("Getting users");
@@ -23,7 +33,9 @@ class _HomeScreenState extends State<HomeScreen> {
           'SELECT * FROM users WHERE uid="${FirebaseAuth.instance.currentUser.uid}"'
     };
     http.Response response = await http.post(userurl, body: sql);
+    print(response);
     var result = jsonDecode(response.body);
+
     if (result[0]['gender'] == "Male") {
       http.Response response = await http.post(userurl,
           body: {'sql': 'SELECT * FROM users WHERE gender="Female"'});
@@ -35,9 +47,132 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  getcurrentuserdata() async {
+    print("Getting currengt user");
+    String userurl = "https://fluttertinderclone.000webhostapp.com/getdata.php";
+    Map sql = {
+      'sql':
+          'SELECT * FROM users WHERE uid="${FirebaseAuth.instance.currentUser.uid}"'
+    };
+    http.Response response = await http.post(userurl, body: sql);
+    print(response);
+    var result = jsonDecode(response.body);
+    setState(() {
+      username = result[0]["name"];
+      bio = result[0]["bio"];
+      profilepic = result[0]["profilepic"];
+    });
+  }
+
+  congratsdialog(String name, String recieverprofilepic) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              decoration: BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Colors.pink, Colors.purple])),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "It is a MATCH",
+                    style: mystyle(20, Colors.white, FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          width: 127,
+                          height: 150,
+                          image: NetworkImage(recieverprofilepic),
+                        ),
+                      ),
+                      Image(
+                        width: 45,
+                        height: 45,
+                        image: AssetImage('images/love.png'),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          width: 127,
+                          height: 150,
+                          image: NetworkImage(profilepic),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "You both have liked each other",
+                    textAlign: TextAlign.center,
+                    style: mystyle(16, Colors.white, FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  likeuser(String uid, String name, String recieverprofilepic) async {
+    print("Getting likes");
+    String currentuserid = FirebaseAuth.instance.currentUser.uid;
+    String api = "https://fluttertinderclone.000webhostapp.com/getdata.php";
+    Map getlikessql = {
+      'sql':
+          'SELECT * FROM likes WHERE liker="$currentuserid" AND reciever="$uid"'
+    };
+    http.Response response = await http.post(api, body: getlikessql);
+    List getlikeslist = jsonDecode(response.body);
+    if (getlikeslist.length == 0) {
+      Map likesql = {
+        'sql':
+            'INSERT INTO likes (id,liker,reciever,name,profilepic,bio) VALUES (NULL,"$currentuserid", "$uid","$username","$profilepic","$bio")'
+      };
+      http.Response response = await http.post(api, body: likesql);
+      SnackBar snackBar = SnackBar(
+          content: Text(
+        "Your like was added",
+        style: mystyle(20, Colors.white, FontWeight.w700),
+      ));
+      scaffoldkey.currentState.showSnackBar(snackBar);
+    } else {
+      print("Already Liked");
+      SnackBar snackBar = SnackBar(
+          content: Text(
+        "Already Liked",
+        style: mystyle(20, Colors.white, FontWeight.w700),
+      ));
+      scaffoldkey.currentState.showSnackBar(snackBar);
+    }
+
+    // Check if both have liked
+    print("Checking if both have liked");
+    Map checkbothsql = {
+      'sql':
+          'SELECT * FROM likes WHERE (liker="$currentuserid" AND reciever ="$uid") OR (liker="$uid" AND reciever ="$currentuserid")'
+    };
+    Response checkbothresponse = await http.post(api, body: checkbothsql);
+    List checkbothresponse_result = jsonDecode(checkbothresponse.body);
+    if (checkbothresponse_result.length == 2) {
+      congratsdialog(name, recieverprofilepic);
+    } else {
+      print("No did not like eachother");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       backgroundColor: Color(0xFFEDF0F6),
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -127,7 +262,10 @@ class _HomeScreenState extends State<HomeScreen> {
                                       color: Colors.red,
                                     ),
                                     iconSize: 32,
-                                    onPressed: () {},
+                                    onPressed: () => likeuser(
+                                        snapshot.data[index]['uid'],
+                                        snapshot.data[index]['name'],
+                                        snapshot.data[index]['profilepic']),
                                   )
                                 ],
                               ),
