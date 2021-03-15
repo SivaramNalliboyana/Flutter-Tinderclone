20c8770e0461a1fb47341b61cdb59676d23071d4
GrandmasterSivaram <nsivaramdav@gmail.com>
2021-03-15 16:43:54 +0100

    Added editprofile

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tinderclone/utils/fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:tinderclone/utils/variables.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -15,6 +19,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   String profilepic;
   String bio;
   bool dataisThere = false;
+  var imagepath;
+  TextEditingController usernamecontroller = TextEditingController();
 
   @override
   void initState() {
@@ -48,6 +54,171 @@ class _ProfileScreenState extends State<ProfileScreen> {
     var response = await http.post(apiurl, body: sql);
     var result = jsonDecode(response.body);
     return result;
+  }
+
+  pickimage(ImageSource source) async {
+    Navigator.pop(context);
+    final imagefile = await ImagePicker()
+        .getImage(source: source, maxHeight: 680, maxWidth: 970);
+    setState(
+      () {
+        imagepath = File(imagefile.path);
+      },
+    );
+  }
+
+  optionsdialog() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            children: [
+              SimpleDialogOption(
+                onPressed: () => pickimage(ImageSource.gallery),
+                child: Text(
+                  "Choose from gallery",
+                  style: mystyle(20, Colors.pink, FontWeight.w700),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => pickimage(ImageSource.camera),
+                child: Text(
+                  "Choose from camera",
+                  style: mystyle(20, Colors.pink, FontWeight.w700),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => Navigator.pop(context),
+                child: Text(
+                  "Cancel",
+                  style: mystyle(20, Colors.pink, FontWeight.w700),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  uploadimage() async {
+    UploadTask storageUploadtask =
+        imagesbucket.child(username).putFile(imagepath);
+    TaskSnapshot storageUploadsnapshot =
+        await storageUploadtask.whenComplete(() {});
+    String downloadurl = await storageUploadsnapshot.ref.getDownloadURL();
+    return downloadurl;
+  }
+
+  updateprofile() async {
+    String url = 'https://fluttertinderclone.000webhostapp.com/edituser.php';
+    String userid = FirebaseAuth.instance.currentUser.uid;
+    // only name
+    if (usernamecontroller.text != '' && imagepath == null) {
+      Map query = {
+        'sql':
+            'UPDATE users SET name="${usernamecontroller.text}" WHERE uid="$userid"'
+      };
+      var post = await http.post(url, body: query);
+      setState(() {
+        username = usernamecontroller.text;
+      });
+      usernamecontroller.clear();
+      Navigator.pop(context);
+    }
+
+    // only image
+    if (usernamecontroller.text == '' && imagepath != null) {
+      String downloadurl = await uploadimage();
+      Map query = {
+        'sql': 'UPDATE users SET profilepic="$downloadurl" WHERE uid="$userid"'
+      };
+      var post = await http.post(url, body: query);
+      setState(() {
+        profilepic = downloadurl;
+      });
+      Navigator.pop(context);
+    }
+
+    // name and image
+    if (usernamecontroller.text != '' && imagepath != null) {
+      String downloadurl = await uploadimage();
+      Map query = {
+        'sql':
+            'UPDATE users SET profilepic="$downloadurl", name="${usernamecontroller.text}" WHERE uid="$userid"'
+      };
+      var post = await http.post(url, body: query);
+      setState(() {
+        username = usernamecontroller.text;
+        profilepic = downloadurl;
+      });
+      usernamecontroller.clear();
+      Navigator.pop(context);
+    }
+  }
+
+  openeditdialog() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            height: 250,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                imagepath == null
+                    ? IconButton(
+                        icon: Icon(Icons.add_a_photo,
+                            color: Colors.pink, size: 55),
+                        onPressed: () => optionsdialog(),
+                      )
+                    : InkWell(
+                        onTap: () => optionsdialog(),
+                        child: CircleAvatar(
+                          backgroundImage: FileImage(imagepath),
+                          radius: 32,
+                        ),
+                      ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  child: TextField(
+                    controller: usernamecontroller,
+                    decoration: InputDecoration(
+                        hintText: "Edit name",
+                        hintStyle: mystyle(20, Colors.black, FontWeight.w600)),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                InkWell(
+                  onTap: () => updateprofile(),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                        colors: [
+                          Color(0xFFFB9245),
+                          Color(0xFFF54E68),
+                        ],
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("Update",
+                          style: mystyle(20, Colors.white, FontWeight.bold)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -120,22 +291,25 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         SizedBox(
                           height: 20,
                         ),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 2,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            gradient: LinearGradient(
-                              colors: [
-                                Color(0xFFFB9245),
-                                Color(0xFFF54E68),
-                              ],
+                        InkWell(
+                          onTap: () => openeditdialog(),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width / 2,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color(0xFFFB9245),
+                                  Color(0xFFF54E68),
+                                ],
+                              ),
                             ),
-                          ),
-                          child: Center(
-                            child: Text("Edit Profile",
-                                style:
-                                    mystyle(20, Colors.white, FontWeight.bold)),
+                            child: Center(
+                              child: Text("Edit Profile",
+                                  style: mystyle(
+                                      20, Colors.white, FontWeight.bold)),
+                            ),
                           ),
                         ),
                         SizedBox(height: 15),
